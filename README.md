8d51b6d2453fc6735e6f5ac79009d6478b4c1b11
Raul Ferras <clinisbut@gmail.com>
2012-01-04 08:55:54 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,81 @@
+jQuery Incromentor: an Input Number fallback/improver
+=====================================================
+
+Overview
+--------
+incromentor is a jQuery plugin that acts as a fallback for those browsers that still doesn't support the new HTML5 input number element.
+Also adds an improvement allowing you to use a callback for every increment/decrement event. This is usefull when you need a non-linear step.
+
+
+Requeriments
+------------
+incromentor requires Modenrizr in order to detect if browser supports the input number element.
+
+
+Usage
+-----
+Include in your site the jquery and Modernizr scripts prior to incromentor:
+
+	<script type="text/javascript" src="jquery-1.6.3.min.js"></script>
+	<script type="text/javascript" src="modernizr.js"></script>
+	<script type="text/javascript" src="incromentor.js"></script>
+
+Include also the stylesheet to customize the appearance of controls:
+
+	<link href="incromentor.css" rel="stylesheet" type="text/css" />
+
+To start using it:
+	$('#the_input').incromentor();
+
+Will simply add support for no-number-browsers.
+Incromentor supports `max`, `min` and `step` html arguments, although these can be also configured from javascript using the options argument:
+
+	$('#the_input').incromentor({
+		max:0,
+		min:10,
+		step:5,
+	});
+
+Available options:
+	* `max`:		Sets maximum value input element can contain. Increment control will be disabled after reaching maximum value 
+					This parameter, if defined will override html max attribute.
+
+	* `min`				:	Sets minimum value input element can contain. Decrement control will be disabled after reaching minimum value.
+							This parameter, if defined will override html max attribute.
+
+	* `step`			:	Decrement/Increment actions will substract/add 5 units to value. By default 1.
+	* `class`			:	Class to be applied to the <span> that wraps buttons. Remember to define this key between double quotes!. 
+							Default value is `incromentor`. `String`. Only used in non-number-browsers!
+	* `more_text`		:	String to place inside increment button. Default is `&#9650`. Only used in non-number-browsers!
+	* `less_text`		:	String to place inside decrement button. Default is `&#9660`. Only used in non-number-browsers!
+	* `keyboard_input`	:	If false, values can only be introduced using UI. `Boolean`, Default `true`.
+	* `stepUp`			:	Callback to be called when user wants to increment the value. If not defined, incromentor uses +`step`.
+	* `stepDown`		:	Callback to be called when user wants to decrement the value. If not defined, incromentor uses -`step`.
+	* `onStepUp`		:	Callback to be called after value has been incremented.
+	* `onStepDown`		:	Callback to be called after value has been decremented.
+
+
+You can configure a function for increment/decrement events:
+
+	$('#the_input').incromentor({
+		
+		stepUp: function( current_value ){
+			return current_value*2;
+		}
+
+		stepDown : function( current_value ){
+			
+			return (current_value==0)? 0:current_value/2;
+		}
+
+	});
+
+
+Styling
+-------
+Incromentor adds the class `incromentor` to every touched element. You can use included `incromentor.css` or create your own one.
+However you can tell incromentor to set a different class name:
+
+	$('#the_input').incromentor({
+		"class":"my-class-name"		//Use double quotes between `class` as it's a javascript keyword!
+	})
---
+++ incromentor.js
@@ -0,0 +1,206 @@
+/*
+	Plugin to add controls to increment/decrement the numeric value
+	of an input text.
+*/
+(function( $ ) {
+
+	$.fn.incromentor = function( options ){
+		
+		return this.each( function(){
+			
+			var $this = $(this),
+				data = $this.data('incromentor');
+			
+			if( !data )
+			{
+				//Init item
+				data = {
+				//	min			: 0,
+				//	max			: 65535,
+				//	step		: 1,
+					"class"			: 'incromentor',
+					more_text			: '&#9650',
+					less_text		: '&#9660',
+					keyboard_input	: true,
+					stepUp			: _calculate_step_up,		//function that calculates next step
+					stepDown		: _calculate_step_down,		//function that calculates previous step
+					onStepUp 		: function(){},				//function to execute after stepping up
+					onStepDown		: function(){},				//function to execute after stepping down
+					previous		: $this.val()				//Previous value
+				};
+
+				var t = $this.attr('max');
+				if( t===undefined && options.max )
+				{
+					$this.attr('max',options.max);
+				}
+				else
+					data.max = ( t===undefined )? null:t;
+
+				t = $this.attr('min');
+				if( t===undefined && options.min )
+				{
+					$this.attr('min',options.min);
+				}
+				else
+					data.min = (t===undefined)? null:t;
+
+				t = $this.attr('step');
+				if( t===undefined && options.step )
+				{
+					$this.attr('step',options.step );
+				}
+				else
+					data.step = (t===undefined)? 1:t;
+
+				data.min = parseInt( data.min, 10 );
+				data.max = parseInt( data.max, 10 );
+				data.toolset = $('<span/>',{
+										"class":data["class"]
+										});
+				data.toolset.up		= $('<button type="button">'+data.more_text+'</button>' ).attr({"class":"btn"});
+				data.toolset.down	= $('<button type="button">'+data.less_text+'</button>' ).attr({"class":"btn"});
+
+				if( Modernizr.inputtypes.number )
+				{
+					$this.bind('change', function(e){
+						
+						var $this		= $(e.target),
+							 data		= $this.data('incromentor'),
+							 current	= parseInt( $this.val(), 10 );
+
+						if( data.previous > $this.val() )
+						{
+							//Value decremented
+							$this.val( data.previous );
+							$this.trigger( 'incromentor.stepdown' );
+						}
+						else if( data.previous < $this.val() )
+						{
+							//Value incremented
+							$this.val( data.previous );
+							$this.trigger( 'incromentor.stepup' );
+						}
+
+						e.preventDefault();
+					});
+				}
+				else
+				{
+					data.toolset.append( data.toolset.up );
+					data.toolset.append( data.toolset.down );
+
+					data.toolset.up.click( function(){
+						$this.trigger( 'incromentor.stepup' );
+					});
+
+					data.toolset.down.click( function(){
+						$this.trigger( 'incromentor.stepdown' );
+					});
+
+					$this.wrap( '<span style="position:relative;display:inline-block;"></span>' );
+					$this.after( data.toolset );
+					$this.bind( 'keydown', _keyboard_handle );
+				}
+
+				
+
+				$this.bind( 'incromentor.stepup', _step_up );
+				$this.bind( 'incromentor.stepdown', _step_down );
+
+				data = $.extend( data, options );
+
+				$this.data('incromentor', data );
+			}
+		});
+	}
+
+
+	function _keyboard_handle( e )
+	{
+		var $this = $(e.target);
+		// ↑ ↓ keys
+		if( e.keyCode===38 )
+		{
+			$this.trigger( 'incromentor.stepup' );
+		}
+
+		if( e.keyCode===40 )
+		{
+			$this.trigger( 'incromentor.stepdown' );
+		}
+
+		if( $this.data('incromentor').keyboard_input===false )
+		{
+			e.preventDefault();
+		}
+	}
+
+
+	function _buttons_state( value, data )
+	{
+		data.toolset.up.attr('disabled', ( value == data.max ) );
+		data.toolset.down.attr('disabled', ( value == data.min ) );		
+	}
+
+
+	function _step_up(){
+
+		var $this	= $(this),
+		data		= $this.data('incromentor'),
+		value		= null,
+		current_val	= parseInt( $this.val(), 10 );
+
+		if( $.isFunction( data.stepUp ) && current_val<data.max )
+		{
+			value = data.stepUp( current_val );
+			$this.trigger('incromentor.onstepup', null );
+
+			$this.val( value );
+			data.previous = parseInt( $this.val(), 10 );
+			$this.data('increment', data );
+		}
+
+		_buttons_state( $this.val(), data );
+	}
+
+
+
+	function _step_down(){
+
+		var $this	= $(this),
+		data		= $this.data('incromentor'),
+		value		= null,
+		current_val	= parseInt( $this.val(), 10 );
+
+		if( $.isFunction( data.stepDown ) && current_val>data.min )
+		{
+			value = data.stepDown( $this.val() );
+			$this.trigger('incromentor.onstepdown', null );
+
+			$this.val( value );
+			data.previous = parseInt( $this.val(), 10 );
+			$this.data('increment', data );
+		}
+
+		_buttons_state( $this.val(), data );
+	}
+
+		
+
+
+		function _calculate_step_up( current_value )
+		{
+			//console.log( 'incrementing '+current_value + '+'+this.step );
+			return current_value + this.step;
+		}
+
+
+		function _calculate_step_down( current_value )
+		{
+			//console.log( 'decrementing '+current_value + '-'+this.step );
+			return current_value - this.step;
+		}
+
+
+})( jQuery );
